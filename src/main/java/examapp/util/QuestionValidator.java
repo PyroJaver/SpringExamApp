02490fa4c983e2c0fbaa9a84450f5e13ddc13db4
PyroJaver <mipleswork@gmail.com>
2022-11-03 15:50:01 +0300

    Implemented validation of questions before adding

--- src/main/java/examapp/util/QuestionValidator.java
+++ src/main/java/examapp/util/QuestionValidator.java
@@ -14,6 +14,18 @@ public class QuestionValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-
+    Question question = (Question) target;
+    if (question.getQuestionText().length()<10){
+        errors.rejectValue("questionText", "question.error.question_text.less_10");
+    }
+    if (question.getQuestionText().length()>1000){
+            errors.rejectValue("questionText", "question.error.question_text.over_1000");
+    }
+    if (question.getAnswer().length()<10){
+            errors.rejectValue("answer", "question.error.answer.less_10");
+    }
+    if (question.getAnswer().length()>1000) {
+            errors.rejectValue("answer", "question.error.answer.over_1000");
+    }
     }
 }
