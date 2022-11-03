fbf3f935524c651f9676e382870ec04a40fe83ad
PyroJaver <mipleswork@gmail.com>
2022-11-03 14:25:05 +0300

    CRUD operations on questions and users implemented

--- src/main/java/examapp/controllers/AdminController.java
+++ src/main/java/examapp/controllers/AdminController.java
@@ -3,37 +3,41 @@ package examapp.controllers;
 import examapp.models.Question;
 import examapp.models.User;
 import examapp.services.QuestionService;
+import examapp.services.UserDetailService;
 import examapp.util.QuestionValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+
+//TODO: этот контроллер чересчур разросся,нужен рефакторинг. Выделить контроллеры пользователей и вопросов.
 @Controller
-@RequestMapping("user/admin")
+@RequestMapping("/admin")
 public class AdminController {
     private final QuestionValidator questionValidator;
     private final QuestionService questionService;
+    private final UserDetailService userDetailService;
 
     @Autowired
-    public AdminController(QuestionValidator questionValidator, QuestionService questionService) {
+    public AdminController(QuestionValidator questionValidator, QuestionService questionService,
+                           UserDetailService userDetailService) {
         this.questionValidator = questionValidator;
         this.questionService = questionService;
+        this.userDetailService = userDetailService;
     }
 
     @GetMapping("/adminPage")
     public String adminPanel(){
-        return "/user/admin/adminPage";
+        return "/admin/adminPage";
     }
 
     @GetMapping("/newQuestion")
     public String newQuestionPage(@ModelAttribute("question") Question question){
-        return "/user/admin/newQuestion";
+        return "/admin/newQuestion";
     }
 
     @PostMapping("/newQuestion")
@@ -41,10 +45,52 @@ public class AdminController {
                                 BindingResult bindingResult){
         questionValidator.validate(question, bindingResult);
         if (bindingResult.hasErrors()){
-            return "/user/admin/newQuestion";
+            return "/admin/newQuestion";
         }
         questionService.saveQuestion(question);
-        return "/user/admin/newQuestion";
+        return "/admin/newQuestion";
+    }
+    @GetMapping("/usersList")
+    public String openUsersList(Model model){
+        model.addAttribute("users", userDetailService.loadAllUsers() ) ;
+        return "/admin/userList";
+    }
+    @DeleteMapping ("/usersList/{id}")
+    public String deleteUserByAdmin(@PathVariable("id") int id){
+        userDetailService.deleteUserById(id);
+        return "redirect:/home";
+    }
+    @GetMapping("/questionList")
+    public String openQuestionsList(Model model){
+        model.addAttribute("questions", questionService.getAllQuestions()) ;
+        return "/admin/questionList";
+    }
+    @DeleteMapping ("/questionList/{id}")
+    public String deleteQuestion(@PathVariable("id") int id){
+        questionService.deleteQuestionById(id);
+        return "redirect:/home";
+    }
+    @GetMapping ("/questionList/{id}")
+    public String editQuestionPage(Model model, @PathVariable("id") int id){
+        model.addAttribute("question", questionService.findById(id));
+        return "/admin/editQuestion";
     }
 
+    @PatchMapping("/questionList/{id}")
+    public String editQuestion(@ModelAttribute Question question, @PathVariable("id") int id){
+    questionService.update(id, question);
+    return "/user/home";
+    }
+
+    @GetMapping ("/userList/{id}")
+    public String editUserPage(Model model, @PathVariable("id") int id){
+        model.addAttribute("user", userDetailService.findById(id));
+        return "/admin/editUser";
+    }
+
+    @PatchMapping("/userList/{id}")
+    public String editUser(@ModelAttribute User user, @PathVariable("id") int id){
+        userDetailService.update(id, user);
+        return "/user/home";
+    }
 }
