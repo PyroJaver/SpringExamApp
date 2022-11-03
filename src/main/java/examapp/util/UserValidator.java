580e1d9a4dad5c82dc7b1f14d81ce7fbdaea0c95
PyroJaver <mipleswork@gmail.com>
2022-11-03 21:17:23 +0300

    User validation implemented

--- src/main/java/examapp/util/UserValidator.java
+++ src/main/java/examapp/util/UserValidator.java
@@ -3,15 +3,18 @@ package examapp.util;
 import examapp.models.User;
 import examapp.services.UserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
+
+import java.util.Objects;
 
 @Component
 public class UserValidator implements Validator {
 
     private final UserDetailService userDetailService;
+
     @Autowired
     public UserValidator(UserDetailService userDetailService) {
         this.userDetailService = userDetailService;
@@ -24,12 +27,35 @@ public class UserValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-     User user = (User)target;
-    try{
-        userDetailService.loadUserByUsername(user.getUsername());
-    }catch (UsernameNotFoundException ignored){
-        return; //All is OK, user not found
-    }
-    errors.rejectValue("username", "", "Человек с таким именем пользователя уже существует");
+        User user = (User) target;
+
+        //Username and password can't me empty or contain whitespace
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "error.not_empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "error.not_empty");
+
+        // Username must have from 4 characters to 32
+        if (user.getUsername().length() < 4) {
+            errors.rejectValue("username", "registration.error.username.less_4");
+        }
+
+        if (user.getUsername().length() > 32) {
+            errors.rejectValue("username", "registration.error.username.over_32");
+        }
+
+        //Username can't be duplicated
+        if (userDetailService.getUserByUsername(user.getUsername()).isPresent()) {
+            errors.rejectValue("username", "registration.error.duplicated.username");
+        }
+
+        //Password must have at least 8 characters and max 32
+        if (user.getPassword().length() < 8) {
+            errors.rejectValue("password", "registration.error.password.less_8");
+        }
+
+        if (user.getPassword().length() > 32){
+            errors.rejectValue("password", "registration.error.password.over_32");
+        }
+
+
     }
 }
