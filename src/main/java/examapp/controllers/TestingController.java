0386bfa6ac833056aca7fe885137bacfbb18f345
PyroJaver <mipleswork@gmail.com>
2022-11-03 19:37:01 +0300

    Request Mapping refactored

--- src/main/java/examapp/controllers/TestingController.java
+++ src/main/java/examapp/controllers/TestingController.java
@@ -10,44 +10,51 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/")
 public class TestingController {
     private final QuestionService questionService;
     private final TestDto testDto;
 
-    public TestingController(QuestionService questionService,  TestDto testDto) {
+    public TestingController(QuestionService questionService, TestDto testDto) {
         this.questionService = questionService;
         this.testDto = testDto;
     }
 
-    @GetMapping("/testingPage")
-    public String testingPage(Model model){
+    @GetMapping("/user/testingPage")
+    public String testingPage(Model model) {
         testDto.setQuestions(questionService.getNewTestQuestions());
         model.addAttribute("test", testDto);
         return "/testing/testingPage";
     }
 
-    @PostMapping("/testingPage")
-    public String saveAnsweredQuestions(@ModelAttribute("test") TestDto testDto ){
+    @PostMapping("/user/testingPage")
+    public String saveAnsweredQuestions(@ModelAttribute("test") TestDto testDto) {
         questionService.setAnsweredQuestions(testDto.getQuestions());
 
         return "redirect:/user/testResults";
     }
-    @GetMapping("/testResults")
-    public String showTestResults(@ModelAttribute("testResult") TestDto testDto){
-        List<Question> testQuestions = questionService.getTestQuestions();
-        List<Question> answeredQuestions = questionService.getAnsweredQuestions();
 
-        //сортировка необходима, чтобы for each и итератор шли по спискам вопросов синхронно
+    @GetMapping("/user/testResults")
+    public String showTestResults(@ModelAttribute("testResult") TestDto testDto) {
+        ArrayList<Question> testQuestions = (ArrayList<Question>) questionService.getTestQuestions();
+        ArrayList<Question> answeredQuestions = (ArrayList<Question>) questionService.getAnsweredQuestions();
+
+        //хоть сами вопросы не будут использованы в представлении, они нужны для вычисления процента выполнения теста
+        testDto.setQuestions(questionService.getTestQuestions());
+
+        //сортировка необходима, чтобы идти по исходным и отвеченным вопросам синхронно
         Collections.sort(answeredQuestions);
         Collections.sort(testQuestions);
 
-        //ответ считается правильным, если поле answer для двух вопросов полностью совпадает.
-        //TODO: реализовать игнорирование регистра букв и одной опечатки в строке
-        for ( Question question: answeredQuestions){
-            if(question.getAnswer()==testQuestions.iterator().next().getAnswer()){
-             testDto.incrementRate();
-        }
+
+        for (int i = 0; i < testQuestions.size(); i++) {
+            //ответ считается правильным, если поле answer для двух вопросов полностью совпадает
+            //регистр букв игнорируется
+            if (Objects.equals(testQuestions.get(i).getAnswer().toLowerCase(),
+                    answeredQuestions.get(i).getAnswer().toLowerCase())) {
+                testDto.incrementRate();
+            }
+
         }
         return "testing/testResults";
     }
