82359b2e77d3c7e001e87d237f51181e6f8e8f06
PyroJaver <mipleswork@gmail.com>
2022-11-03 20:05:49 +0300

    user controller blank. The methods that work with the user will be moved here from the AdminController class

--- src/main/java/examapp/controllers/UserController.java
+++ src/main/java/examapp/controllers/UserController.java
@@ -1,41 +1,30 @@
 package examapp.controllers;
 
-import examapp.models.Question;
-import examapp.services.QuestionService;
+import examapp.models.User;
+import examapp.services.UserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Set;
 
 @Controller
 @RequestMapping("/")
 public class UserController {
-    @Value("${questions.sizeOfTest}") int sizeOfTest;
+    private final UserDetailService userDetailService;
+
     @Autowired
-    private final QuestionService questionService;
-
-    public UserController( QuestionService questionService) {
-
-        this.questionService = questionService;
+    public UserController(UserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
     }
 
-    @GetMapping("/user/home")
+    @GetMapping("/home")
     public String homePage(){
         return "/user/home";
     }
 
-    @GetMapping("/user/testingPage")
-    public String testingPage(Model model){
-        List<Question> questions = questionService.getSomeQuestions();
-        model.addAttribute("questions", questions);
-        return "/testing/testingPage";
+    @DeleteMapping("/{id}")
+    public String deleteUserByUser(@ModelAttribute User user){
+        userDetailService.deleteUserByEntity(user);
+        return "redirect:/home";
     }
-
-
 }
