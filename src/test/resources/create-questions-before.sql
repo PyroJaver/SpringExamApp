b9f712353031f8232538ab8dd17c621390a7d8fa
PyroJaver <mipleswork@gmail.com>
2022-12-05 17:09:08 +0300

    SQL commands for testing purposes implemented

---
+++ src/test/resources/create-questions-before.sql
@@ -0,0 +1,6 @@
+TRUNCATE table questions;
+
+insert into questions(id, questionText, answer) values
+(1, 'testQuestionText', 'testAnswer'),
+(2, 'testQuestionText2', 'testAnswer2'),
+(3, 'testQuestionText3', 'testAnswer3');
